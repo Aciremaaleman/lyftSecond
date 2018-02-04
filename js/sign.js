e2b631ad24c47a9bf08c84a65efe4115e8f8a386
aciremaaleman <aciremaaleman@outlook.es>
2018-02-04 13:53:40 -0600

    Actualizando lyft

--- js/sign.js
+++ js/sign.js
@@ -1,27 +1,57 @@
 var $input = $("#input");
-var $btn = $("#btn");
+var $inputCode = $("#code");
+var $resendCode = $("#btnCode");
+var $validCode = Math.floor(Math.random() * 1000);
+// var $name = $("#name");
+// var $password = $("#password");
+// var $check = $("#check");
 
-function(loadPage){
-  $("#input").keyup(valid);
-}
+$(document).ready(function() {
+  $input.keyup(valid);
+  $inputCode.keyup(activeBoton);
+  $resendCode.click(generateCode);
+  // $name.keyup(form);
+  // $password.keyup(form);
+  // $check.keyup(form);
 
-function valid () {
-    if ($("#input").val().length === 10) {
-        return $("#btn").prop("disabled", false)
-    }
-    else {
-        $("#btn").prop("disabled", true)
-    }
+});
+
+var valid = function() {
+  if ($("#input").val().trim().length == 10) {
+    $("#btn").addClass('active');
+    $("#btn").removeClass('disabled');
+    $("#btn").click(function() {
+      location.href = "../views/verify.html";
+      alert("Lab- " + $validCode);
+    });
+  };
 };
 
-// function generateCode (e) {
-//     e.preventDefault();
-//     var validCode = Math.floor(Math.random() * 1000);
-//    // console.log(code)
-//    alert("LAB- " + validCode)
-//    // redereccion a siguiente vista, al cerrar alert
-//    location.href = "verify.html";
+function generateCode() {
+  var validCode = Math.floor(Math.random() * 1000);
+  console.log(validCode);
+  alert("LAB- " + validCode);
+  // // redereccion a siguiente vista, al cerrar alert
+  // location.href = "verify.html";
+};
+
+var activeBoton = function() {
+  if ($("#code").val().trim().length == 3) {
+    $("#btnVerify").addClass('active');
+    $("#btnVerify").removeClass('disabled');
+    $("#btnVerify").click(function() {
+      location.href = "../views/form.html";
+
+    });
+  };
+};
+
+// var form = function() {
+//   if($("#name").val().trim().length > 0 && $("#password").val().trim().length > 0 && $("#check").prop('checked', true) {
+//     $("#last-btn").addClass('active');
+//     $("#last-btn").removeClass('disabled');
+//     $("#last-btn").click(function() {
+//       location.href = "../views/check.html";
+//     });
+//   });
 // };
-
-
-$(document).ready(loadPage);
